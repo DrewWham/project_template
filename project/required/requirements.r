35a07cef485a122c5c5de64fb11427f726f1679a
DrewWham <Fcwham@gmail.com>
2026-09-01 12:40:49 -0400

    Update requirements.r

--- project/required/requirements.r
+++ project/required/requirements.r
@@ -1,3 +1,28 @@
-install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
-install.packages("optparse",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+# requirements.R
+#
+# Install R packages commonly used in STAT 380 projects.
+#
+# Run with:
+#   Rscript required/requirements.R
 
+required_packages <- c(
+  "data.table",
+  "glmnet",
+  "xgboost"
+)
+
+installed_packages <- rownames(installed.packages())
+
+packages_to_install <- setdiff(
+  required_packages,
+  installed_packages
+)
+
+if (length(packages_to_install) > 0) {
+  install.packages(
+    packages_to_install,
+    repos = "https://cloud.r-project.org"
+  )
+}
+
+message("Required R packages are installed.")
